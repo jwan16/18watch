c22deab797f53e1127d8b911bef0a699a0ce5890
CGG <wanyichun@gmail.com>
2017-08-14 11:17:09 +0800

    update static

--- static/watch/js/ajax.js
+++ static/watch/js/ajax.js
@@ -33,22 +33,6 @@ $(function() {
         });
 
 
-$(document).ready(function() {
-    var filter_price_max = $( "#slider-range" ).slider( "values", 1 );
-    var filter_price_min = $( "#slider-range" ).slider( "values", 0 )
-	$.ajax({
-            url: "/search/",
-            type: "POST",
-            async: false,
-            data: {
-                csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken").val(),
-                filter_price_min,
-                filter_price_max
-            },
-            success: function(){
-                 $('#search-results').load('/search/');
-            }
-        });})
 
 
 $('#index_filter').submit(function () {
@@ -69,8 +53,7 @@ $('#index_filter').submit(function () {
              // all data will be passed here
         },
         success: function(data){
-             window.location.assign('/watch');
-             console.log('ajax done')
+             window.location.assign('watch');
         }
     });
 });
